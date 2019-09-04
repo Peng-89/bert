0ffae996cbdb93f9f815d72a20f33ff9adfa10f7
徐鹏 <xu.peng@belle.com.cn>
2019-09-04 10:39:27 +0800

    新增

--- examples/text_classifiler/client.sh
+++ examples/text_classifiler/client.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-export BERT_BASE_DIR=~/bert/bert_model/models/chinese_L-12_H-768_A-12
-export GLUE_DIR=~/bert/examples/text_classifiler/data
-export MODEL_DIR=~/bert/examples/text_classifiler/output
+export BERT_HOME=/content/bert
+export BERT_BASE_DIR=${BERT_HOME}/bert_model/models/chinese_L-12_H-768_A-12
+export GLUE_DIR=${BERT_HOME}/examples/text_classifiler/data
+export MODEL_DIR=${BERT_HOME}/examples/text_classifiler/output
 
-python ~/bert/examples/text_classifiler/client.py \
+python ${BERT_HOME}/examples/text_classifiler/client.py \
   --data_dir=$GLUE_DIR \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
--- examples/text_classifiler/export.sh
+++ examples/text_classifiler/export.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-export BERT_BASE_DIR=~/bert/bert_model/models/chinese_L-12_H-768_A-12
-export GLUE_DIR=~/bert/examples/text_classifiler/data
-export MODEL_DIR=~/bert/examples/text_classifiler/output
-export MODEL_PB_DIR=~/bert/examples/text_classifiler/api/
+export BERT_HOME=/content/bert
+export BERT_BASE_DIR=${BERT_HOME}/bert_model/models/chinese_L-12_H-768_A-12
+export GLUE_DIR=${BERT_HOME}/examples/text_classifiler/data
+export MODEL_DIR=${BERT_HOME}/examples/text_classifiler/output
+export MODEL_PB_DIR=${BERT_HOME}/examples/text_classifiler/api/
 
-python ~/bert/examples/text_classifiler/export.py \
+python ${BERT_HOME}/examples/text_classifiler/export.py \
   --task_name=setiment \
   --do_predict=true \
   --data_dir=$GLUE_DIR/ \
